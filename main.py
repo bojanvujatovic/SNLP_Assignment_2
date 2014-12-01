77e6ae39f4768f815cccfd893554610502e82648
hmourit <hmourit@gmail.com>
2014-12-01 11:43:22 +0000

    Add some useless comments.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """main.py
 
 Main file of the project containing main function. Defines the project 
@@ -10,6 +11,8 @@ from Classes.Sentences import *
 def main():
     """Main function of the project.
     # QUESTO E IL MIO COMMENTO
+    # Esto es un comentario en español
+    # Isto é un comentario en galego
     Defines the project structure calling the following functions:
     1. 
     2. 
