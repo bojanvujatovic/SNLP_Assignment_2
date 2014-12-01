177e67baad0b7f11ee154e1d2e82090bee73018e
mtthss <matteo.hessel@mail.polimi.it>
2014-12-01 18:25:01 +0100

    basic feature function

--- Features/example_feature.py
+++ Features/example_feature.py
@@ -1,5 +1,11 @@
 '''example_feature.py
 '''
 
-def example_feature(token, event_candidate, event_candidate_args):
-    return 0
+import Classes
+
+def example_feature(token, event_candidate, event_candidate_args, compare_word):
+    
+    if token.word == compare_word:
+        return 1
+    else:
+        return 0
