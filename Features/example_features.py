8e2ce54e9fe73259e6bb4bb4effe665f5210ec5e
miljan <miljan@dhcp-14-184.xlate.eduroam.ucl.ac.uk>
2014-12-02 13:41:43 +0000

    added new features

--- Features/example_features.py
+++ Features/example_features.py
@@ -4,7 +4,7 @@
 from scipy.sparse import *
 from scipy import *
 
-def word_template_feature(token, event_candidate, event_candidate_args, word_dict):
+def word_template_feature(event_candidate_args, word_dict, token, event_candidate):
     
     N_words = len(word_dict)
     
@@ -13,8 +13,9 @@ def word_template_feature(token, event_candidate, event_candidate_args, word_dic
     j = array([0])
 
     return csc_matrix((data, (i, j)), shape=(N_words, 1), dtype=int8)
-    
-def word_class_template_feature(token, event_candidate, event_candidate_args, word_dict, class_dict):
+
+# Word belongs to a class                      
+def word_class_template_feature(event_candidate_args, word_dict, class_dict, token, event_candidate):
     
     N_words = len(word_dict)
     N_classes = len(class_dict)
@@ -26,89 +27,92 @@ def word_class_template_feature(token, event_candidate, event_candidate_args, wo
     return csc_matrix((data, (i, j)), shape=(N_words*N_classes, 1), dtype=int8)
 
 # Token has a capital letter
-def capital_leter_feature(token):
+def capital_leter_feature(class_dict, token, event_candidate):
+    
+    N_classes = len(class_dict)
     
     if token.word[0].isupper(): 
         data = array([1])
     else:
         data = array([0])
-    i = array([0])
+    i = array([class_dict[event_candidate]])
     j = array([0])
     
-    return csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(N_classes, 1), dtype=int8)
 
 # Token is in the trigger dictionary
-def token_in_trigger_dict_feature(token, trigger_dict):
+def token_in_trigger_dict_feature(class_dict, trigger_dict, token, event_candidate):
+    
+    N_classes = len(class_dict)
     
     if token.word in trigger_dict:
         data = array([1])
     else:
         data = array([0])
-    i = array([0])
+    i = array([class_dict[event_candidate]])
     j = array([0])
     
-    return csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(N_classes, 1), dtype=int8)
 
 # Token has a number
-def number_in_token_feature(token):
+def number_in_token_feature(class_dict, token, event_candidate):
+    
+    N_classes = len(class_dict)
         
     if any([char.isdigit() for char in token.word]):
-        print "hi"
         data = array([1])
     else:
         data = array([0])
-    i = array([0])
+    i = array([class_dict[event_candidate]])
     j = array([0])
-    print csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8).todense()
-    return csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8)
+
+    return csc_matrix((data, (i, j)), shape=(N_classes, 1), dtype=int8)
 
 # Token is in a protein
-def token_in_protein_feature(token):
+def token_in_protein_feature(class_dict, token, event_candidate):
+    
+    N_classes = len(class_dict)
     
     if ("Protein" or "protein") in token.mentions:
         data = array([1])
     else:
         data = array([0])
-    i = array([0])
+    i = array([class_dict[event_candidate]])
     j = array([0])
     
-    print csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8).todense()
-    return csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(N_classes, 1), dtype=int8)
 
 # Token is after "-"
-def token_is_after_dash_feature(token):
+def token_is_after_dash_feature(class_dict, token, event_candidate):
+    
+    N_classes = len(class_dict)
     
     if token.word[0] == "-": 
         data = array([1])
     else:
         data = array([0])
-    i = array([0])
+    i = array([class_dict[event_candidate]])
     j = array([0])
     
-    return csc_matrix((data, (i, j)), shape=(1, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(N_classes, 1), dtype=int8)
 
 # POS tags combined with class; events are usually nouns, verbs or adjectives
-def pos_class_feature(token, event_candidate, word_dict, class_dict):
-    
+def pos_class_feature(class_dict, token, event_candidate):
+        
     pos_tags = ['NN','NNP','NNS','NNPS',
-                     'VB','VBD','VBG','VBN','VBP','VBZ',
-                     'JJ','JJR','JJS']
-    N_classes = len(class_dict)
-    N_tags = len(pos_tags)
+                'VB','VBD','VBG','VBN','VBP','VBZ',
+                'JJ','JJR','JJS']
     # append the protein entry to the class list
-    class_dict.expand({"Protein":len(class_dict)+1})
+    class_dict.update({"Protein":len(class_dict)+1})
+    N_classes = len(class_dict)
     
     if token.pos in pos_tags:
         data = array([1])
     else:
         data = array([0])
-    i = array([N_classes*N_tags + class_dict[event_candidate]])
+    i = array([class_dict[event_candidate]])
     j = array([0])
-
-    return csc_matrix((data, (i, j)), shape=(N_classes*N_tags, 1), dtype=int8)
-
-# Bigrams of characters
-def character_bigram_feature():
-    return
     
+    return csc_matrix((data, (i, j)), shape=(N_classes, 1), dtype=int8)
+
     
