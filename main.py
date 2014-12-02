84da4f48f6e9231e362ff171baf0a2c28f4dc072
miljan <miljan@dhcp-14-184.xlate.eduroam.ucl.ac.uk>
2014-12-02 13:42:18 +0000

    tests

--- main.py
+++ main.py
@@ -32,8 +32,30 @@ def main():
     print trigger_dict
     
 #     word_class_template_feature(train_sentences.sentences[1].tokens[1], "None", None, word_dict, class_dict)
-    print train_sentences.sentences[1].tokens[0].word
-    number_in_token_feature(train_sentences.sentences[1].tokens[0])
+    
+def mainmiljan():
+    all_train_sentences = Paragraphs("Dataset/Train_small/").all_sentences()
+    (train_sentences, hand_out_sentences) = all_train_sentences.split_randomly(0.8)
+    
+    #test_sentences = Paragraphs("Dataset/Test/").all_sentences()
+    
+    word_dict = train_sentences.get_word_dict()
+    class_dict = train_sentences.get_class_dict()
+    trigger_dict = train_sentences.get_trigger_dict()
+    
+    print class_dict
+    print trigger_dict
+      
+    for token in train_sentences.sentences[0].tokens:
+        print token.word + " "
+#     print trigger_dict["or"]
+#     print train_sentences.sentences[2].tokens[1].word
+#     token_in_trigger_dict_feature(train_sentences.sentences[2].tokens[1], trigger_dict)
+
+#     pos_class_feature(train_sentences.sentences[0].tokens[25], 'None', word_dict, class_dict)
+    
+    print train_sentences.sentences[0].tokens[0].word
+    pos_class_feature(class_dict, train_sentences.sentences[0].tokens[0], "Transcription")
 
 if __name__ == "__main__":
-    main()
+    mainmiljan()
