04d75917361d6e08fd328d7e882baf3ca8c8b132
hmourit <hmourit@gmail.com>
2014-12-03 15:45:35 +0000

    Bug fix.

--- Features/example_features.py
+++ Features/example_features.py
@@ -1,8 +1,8 @@
 '''example_feature.py
 '''
 
-from scipy.sparse import *
-from scipy import *
+from scipy.sparse import csc_matrix, vstack
+from scipy import array
 
 
 def word_template_feature(word_dict, token):
@@ -12,7 +12,7 @@ def word_template_feature(word_dict, token):
     i = array([word_dict[token.word]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_words, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_words, 1))
 
 
 # Word belongs to a class                      
@@ -21,13 +21,14 @@ def word_class_template_feature(word_dict, class_dict, token, event_candidate):
     n_classes = len(class_dict)
 
     data = array([1])
+
     i = array([word_dict[token.word] * n_classes + class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_words * n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_words * n_classes, 1))
 
 # Token has a capital letter
-def capital_leter_feature(class_dict, token, event_candidate):
+def capital_letter_feature(class_dict, token, event_candidate):
     n_classes = len(class_dict)
 
     if token.word[0].isupper():
@@ -37,7 +38,7 @@ def capital_leter_feature(class_dict, token, event_candidate):
     i = array([class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_classes, 1))
 
 
 # Token is in the trigger dictionary
@@ -51,7 +52,7 @@ def token_in_trigger_dict_feature(class_dict, trigger_dict, token, event_candida
     i = array([class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_classes, 1))
 
 
 # Token has a number
@@ -65,7 +66,7 @@ def number_in_token_feature(class_dict, token, event_candidate):
     i = array([class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_classes, 1))
 
 
 # Token is in a protein
@@ -79,7 +80,7 @@ def token_in_protein_feature(class_dict, token, event_candidate):
     i = array([class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_classes, 1))
 
 
 # Token is after "-"
@@ -93,7 +94,7 @@ def token_is_after_dash_feature(class_dict, token, event_candidate):
     i = array([class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_classes, 1))
 
 
 # POS tags combined with class; events are usually nouns, verbs or adjectives
@@ -112,7 +113,7 @@ def pos_class_feature(class_dict, token, event_candidate):
     i = array([class_dict[event_candidate]])
     j = array([0])
 
-    return csc_matrix((data, (i, j)), shape=(n_classes, 1), dtype=int8)
+    return csc_matrix((data, (i, j)), shape=(n_classes, 1))
 
 
 # Ngrams of characters
@@ -126,18 +127,17 @@ def character_ngram_feature(n, ngram_combinations, class_dict, token, event_cand
                for index in [ngram_combinations[ngram] for ngram in ngrams]])
     j = array([0] * len(ngrams))
 
-    # print csc_matrix((data, (i, j)), shape=(n_grams * n_classes, 1), dtype=int8)
-    return csc_matrix((data, (i, j)), shape=(n_grams * n_classes, 1), dtype=int8)
+    # print csc_matrix((data, (i, j)), shape=(n_grams * n_classes, 1))
+    return csc_matrix((data, (i, j)), shape=(n_grams * n_classes, 1))
 
 
 def whole_set_of_features(word_dict, class_dict, trigger_dict, n, ngram_combinations, token, event_candidate):
-    # return vstack([word_template_feature(word_dict, token),
-    #                word_class_template_feature(word_dict, class_dict, token, event_candidate),
-    #                capital_leter_feature(class_dict, token, event_candidate),
-    #                token_in_trigger_dict_feature(class_dict, trigger_dict, token, event_candidate),
-    #                number_in_token_feature(class_dict, token, event_candidate),
-    #                token_in_protein_feature(class_dict, token, event_candidate),
-    #                token_is_after_dash_feature(class_dict, token, event_candidate),
-    #                pos_class_feature(class_dict, token, event_candidate),
-    #                character_ngram_feature(n, ngram_combinations, class_dict, token, event_candidate)])
-    return word_template_feature(word_dict, token)
+    return vstack([word_template_feature(word_dict, token),
+                   word_class_template_feature(word_dict, class_dict, token, event_candidate),
+                   capital_letter_feature(class_dict, token, event_candidate),
+                   token_in_trigger_dict_feature(class_dict, trigger_dict, token, event_candidate),
+                   number_in_token_feature(class_dict, token, event_candidate),
+                   token_in_protein_feature(class_dict, token, event_candidate),
+                   token_is_after_dash_feature(class_dict, token, event_candidate),
+                   pos_class_feature(class_dict, token, event_candidate),
+                   character_ngram_feature(n, ngram_combinations, class_dict, token, event_candidate)])
