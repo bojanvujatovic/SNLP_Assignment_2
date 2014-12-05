19201113c3f4363d89e5c73a46a5a866a3330c93
hmourit <hmourit@gmail.com>
2014-12-05 20:03:04 +0000

    Improve StructuredLoglinear.

--- classifier/loglinear/Loglinear.py
+++ classifier/loglinear/Loglinear.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from numpy import argmax, mat, zeros, copy
-
-from scipy.sparse import csc_matrix, csr_matrix
+from numpy import argmax, mat, zeros
 from classifier.ClassifierModel import ClassifierModel, TrainedClassifierModel
 
 
@@ -20,7 +18,7 @@ class LoglinearModel(ClassifierModel):
     def train(self, tokens):
         phi_length = self.phi(tokens[1], self.classes[1]).shape[0]
         weights = mat(zeros((phi_length, 1)))
-
+        # TODO: average weights
         for epoch in range(1, self.max_iterations + 1):
             print 'epoch', epoch
             changed = False
@@ -36,16 +34,15 @@ class LoglinearModel(ClassifierModel):
         return TrainedLoglinearModel(weights, self)
 
 
-class StructuredLoglinearModel(LoglinearModel):
+class NaiveStructuredLoglinearModel(LoglinearModel):
 
     def __init__(self, gold, phi, classes, arguments, alpha, max_iterations=10):
-        super(StructuredLoglinearModel, self).__init__(gold, phi, classes, alpha, max_iterations)
+        super(NaiveStructuredLoglinearModel, self).__init__(gold, phi, classes, alpha, max_iterations)
         self.arguments = arguments
 
     def get_argmax(self, word, weights):
-
         return [self.arguments[argmax([(self.phi(w, t).T * weights)[0, 0] for t in self.arguments])]
-                for w in word.sentence if w != word]
+                for w in word.sentence]# if w != word]
 
         # prediction = []
         # for w in word.sentence:
@@ -56,6 +53,54 @@ class StructuredLoglinearModel(LoglinearModel):
         #     prediction.append(self.arguments[best_index])
         #
         # return prediction
+
+
+class EarlyUpdateStructuredLoglinearModel(LoglinearModel):
+
+    def __init__(self, gold, phi, classes, arguments, alpha, max_iterations=10):
+        super(EarlyUpdateStructuredLoglinearModel, self).__init__(gold, phi, classes, alpha, max_iterations)
+        self.arguments = arguments
+
+    def get_argmax(self, word, weights):
+        truth = self.gold(word)
+        prediction = []
+        for i in range(len(word.sentence)):
+            best_index = argmax([(self.phi(word.sentence[i], t).T * weights)[0, 0] for t in self.arguments])
+            if self.arguments[best_index] != truth[i]:
+                break
+            else:
+                prediction.append(self.arguments[best_index])
+        return prediction
+
+
+class SearchStructuredLoglinearModel(LoglinearModel):
+
+    def __init__(self, gold, phi, classes, arguments, alpha, max_iterations=10):
+        super(SearchStructuredLoglinearModel, self).__init__(gold, phi, classes, alpha, max_iterations)
+        self.arguments = arguments
+
+    def get_argmax(self, word, weights):
+        pred = word.sentence.copy()
+        pred = map(lambda w: (w, 'None', (self.phi(w, 'None')).T * weights)[0, 0], pred)
+
+        first = (0, 'None', float('-inf'))
+        second = (0, 'None', float('-inf'))
+        for i in range(len(pred)):
+            if pred[i][0] == word:
+                continue
+            for arg in ['Cause', 'Theme']:
+                diff = (self.phi(pred[i][0], arg).T * weights)[0, 0] - pred[i][2]
+                if diff > first[2]:
+                    second = first
+                    first = (i, arg, diff)
+                elif diff > second[2]:
+                    second = (i, arg, diff)
+
+        pred = map(lambda p: p[1], pred)
+        if first[2] > float('-inf'):
+            pred[first[0]] = first[1]
+        if second[2] > float('-inf'):
+            pred[second[0]] = second[1]
 
 
 class TrainedLoglinearModel(TrainedClassifierModel):
